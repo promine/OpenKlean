d628c9afca03afd4c766859c2facbf5dc5e5994e
andersonvom <andersonmesquita@promine.com.br>
2009-10-10 16:29:34 -0300

    Authentication in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.login  '/login',  :controller => 'user_sessions', :action => 'new'
+  map.logout '/logout', :controller => 'user_sessions', :action => 'destroy'
+  map.signup '/signup', :controller => 'users', :action => 'new'
+
   map.resources :users
   map.resources :user_sessions
   map.resources :projects
