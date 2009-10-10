13e9e2820022a5b87a0acc2ccef7003be0df6aad
Marcus Holanda <marcushlm@gmail.com>
2009-10-10 15:36:08 -0300

    adding new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :users
   map.resources :projects
+  map.resources :phases
+  map.resources :boards
+  map.resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
 
